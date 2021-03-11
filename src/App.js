2b6e34afed9df2f7c89e4b4d38a3053fc93db766
Denis Karlinsky <64307212+fondunn@users.noreply.github.com>
2021-03-11 06:10:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,65 @@
+// -----------------------------------
+// -----------------------------------
+// ------ With Components ------------
+// -----------------------------------
+// -----------------------------------
+import React from 'react';
+
+import Info from './components/info';
+import Form from './components/form';
+import Weather from './components/weather';
+
+import './App.css';
+
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+
+class App extends React.Component {
+    
+    state = {
+        city:     undefined,
+        temp:     undefined,
+        humidity: undefined,
+        sky:      undefined,
+        saveCity: false
+    }
+
+    getWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+
+        if ( city ) {
+            const API_URL = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            const data = await API_URL.json();
+            this.setState({
+                city:     data.name,
+                temp:     data.main.temp,
+                humidity: data.main.humidity,
+                sky:      data.weather[0].description
+            });
+        } 
+    } 
+
+    render() {
+        return(
+            <div>
+                <Info />
+                <Form weatheMethod={this.getWeather} />
+                <Weather 
+                    city={this.state.city}
+                    temp={this.state.temp}
+                    humidity={this.state.humidity}
+                    sky={this.state.sky}
+                />
+                
+            </div>
+        );
+    }
+    
+}
+export default App;
+
 // import React from 'react';
 
 // import './App.css';
@@ -65,65 +127,3 @@
     
 // }
 // export default App;
-
-// -----------------------------------
-// -----------------------------------
-// ------ With Components ------------
-// -----------------------------------
-// -----------------------------------
-import React from 'react';
-
-import Info from './components/info';
-import Form from './components/form';
-import Weather from './components/weather';
-
-import './App.css';
-
-
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-
-
-class App extends React.Component {
-    
-    state = {
-        city:     undefined,
-        temp:     undefined,
-        humidity: undefined,
-        sky:      undefined,
-        saveCity: false
-    }
-
-    getWeather = async (e) => {
-        e.preventDefault();
-        const city = e.target.elements.city.value;
-
-        if ( city ) {
-            const API_URL = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-            const data = await API_URL.json();
-            this.setState({
-                city:     data.name,
-                temp:     data.main.temp,
-                humidity: data.main.humidity,
-                sky:      data.weather[0].description
-            });
-        } 
-    } 
-
-    render() {
-        return(
-            <div>
-                <Info />
-                <Form weatheMethod={this.getWeather} />
-                <Weather 
-                    city={this.state.city}
-                    temp={this.state.temp}
-                    humidity={this.state.humidity}
-                    sky={this.state.sky}
-                />
-                
-            </div>
-        );
-    }
-    
-}
-export default App;
